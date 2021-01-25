dd61fdd9261abc0ef5151782c6c4bb3b77cc83b7
Pradyumn Goel <pradyum.goel2019@gmail.com>
2021-01-25 17:18:36 +0530

    Republic day

--- Republic-day-special/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-document.querySelector('button').addEventListener('click', function () {
-  document.querySelector('.flag').classList.remove('hidden');
-
-  document.querySelector('.rope').classList.add('hidden');
-  document.querySelector('.salute').classList.remove('hidden');
-});
