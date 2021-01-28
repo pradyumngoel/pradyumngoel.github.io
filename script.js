2880a97b432ba3f1505897a061a8ba1e87e5b23a
Pradyumn Goel <pradyum.goel2019@gmail.com>
2021-01-28 16:12:22 +0530

    Stories

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 
 const gamesTab = document.querySelector('.links-tab-1');
 const programsTab = document.querySelector('.links-tab-2');
+const storiesTab = document.querySelector('.links-tab-3');
 
 const gamesTabContent = document.querySelector('.content1');
 const programsTabContent = document.querySelector('.content2');
+const storiesTabContent = document.querySelector('.content3');
 
 const iconHome = document.querySelector('.ion-ios-home');
 
 gamesTab.addEventListener('click', function () {
   gamesTab.classList.remove('links-tab-active');
   programsTab.classList.remove('links-tab-active');
+  storiesTab.classList.remove('links-tab-active');
 
   gamesTab.classList.add('links-tab-active');
 
   programsTabContent.classList.add('hidden');
+  storiesTabContent.classList.add('hidden');
   gamesTabContent.classList.remove('hidden');
 
   iconHome.classList.remove('hidden-i');
@@ -23,11 +27,26 @@ gamesTab.addEventListener('click', function () {
 programsTab.addEventListener('click', function () {
   gamesTab.classList.remove('links-tab-active');
   programsTab.classList.remove('links-tab-active');
+  storiesTab.classList.remove('links-tab-active');
 
   programsTab.classList.add('links-tab-active');
 
   gamesTabContent.classList.add('hidden');
+  storiesTabContent.classList.add('hidden');
   programsTabContent.classList.remove('hidden');
+
+  iconHome.classList.remove('hidden-i');
+});
+
+storiesTab.addEventListener('click', function () {
+  gamesTab.classList.remove('links-tab-active');
+  programsTab.classList.remove('links-tab-active');
+
+  storiesTab.classList.add('links-tab-active');
+
+  gamesTabContent.classList.add('hidden');
+  programsTabContent.classList.add('hidden');
+  storiesTabContent.classList.remove('hidden');
 
   iconHome.classList.remove('hidden-i');
 });
