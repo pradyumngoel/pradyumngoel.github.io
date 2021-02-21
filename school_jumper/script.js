4a3994fdff8c8638b339a1a904dea49d3ca3f1fc
Pradyumn Goel <pradyum.goel2019@gmail.com>
2021-02-21 11:50:24 +0530

    Scoring in school jumper

--- school_jumper/script.js
+++ school_jumper/script.js
@@ -1,18 +1,26 @@
 const boy = document.querySelector('.boy');
 const fail = document.querySelector('.fail');
+const pass = document.querySelector('.pass');
+const scoreText = document.querySelector('.score');
+
+let score = 0;
 
 const jump = function () {
   if (boy.classList != 'jump') {
     boy.classList.add('jump');
+    pass.style.top = Math.floor(Math.random() * 448 + 448) + 'px';
 
     setTimeout(() => {
       boy.classList.remove('jump');
-    }, 700);
+    }, 400);
   }
 };
 
 let isAlive = setInterval(() => {
   let collided = false;
+
+  score++;
+  scoreText.textContent = `Score: ${score}`;
 
   let playerTop = parseInt(
     window.getComputedStyle(boy).getPropertyValue('top')
@@ -22,13 +30,20 @@ let isAlive = setInterval(() => {
     window.getComputedStyle(fail).getPropertyValue('left')
   );
 
+  let passLeft = parseInt(
+    window.getComputedStyle(pass).getPropertyValue('left')
+  );
+
   if (failLeft < 50 && failLeft > 0 && playerTop >= 448) {
     collided = true;
-    console.log(collided);
 
     if (collided) {
       gameOver();
     }
+  }
+
+  if (failLeft < 50 && failLeft > 0 && playerTop >= 448) {
+    score += 5;
   }
 }, 10);
 
@@ -36,6 +51,7 @@ const gameOver = function () {
   document.querySelector('img').classList.remove('hidden');
   fail.classList.add('hidden');
   boy.classList.add('hidden');
+  pass.classList.add('hidden');
   clearInterval(isAlive);
 };
 
