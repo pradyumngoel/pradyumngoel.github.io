13a266d0e897bb5400800684e8321da7ae0fa762
pradyumngoel <pradyumn.goel2019@gmail.com>
2022-09-11 17:35:25 +0530

    Minor changes

--- projects/letter_arranger/script.js
+++ projects/letter_arranger/script.js
@@ -1,43 +1,43 @@
 // Letter arranger
 const textboxUnorderedLetters = document.querySelectorAll(
-  '.unordered--letters'
+  ".unordered--letters"
 );
-const inputAnswer = document.querySelector('input');
-const buttonCheck = document.querySelector('.check');
-const buttonAgain = document.querySelector('.again');
-const elScore = document.querySelector('.score');
-const elHighScore = document.querySelector('.highscore');
+const inputAnswer = document.querySelector("input");
+const buttonCheck = document.querySelector(".check");
+const buttonAgain = document.querySelector(".again");
+const elScore = document.querySelector(".score");
+const elHighScore = document.querySelector(".highscore");
 
-const msg = document.querySelector('.msg2');
+const msg = document.querySelector(".msg2");
 
-const letters = 'a b c d e f g h i j k l m n o p q r s t u v w q y z'.split(
-  ' '
+const letters = "a b c d e f g h i j k l m n o p q r s t u v w q y z".split(
+  " "
 );
 let score = 0;
 let highscore = 0;
 
 const generateRandomJumbledWord = function () {
-  let jumbled = '';
+  let jumbled = "";
   for (let i = 0; i < 4; i++) {
     jumbled += letters[Math.trunc(Math.random() * letters.length)];
   }
-  document.querySelector('.unordered--letters').textContent = jumbled;
+  document.querySelector(".unordered--letters").textContent = jumbled;
   return jumbled;
 };
 
 let jumbledWord = generateRandomJumbledWord();
 
-buttonCheck.addEventListener('click', function (e) {
+buttonCheck.addEventListener("click", function (e) {
   e.preventDefault();
 
   const answer = inputAnswer.value;
-  const arranged = jumbledWord.split('').sort().join('');
+  const arranged = jumbledWord.split("").sort().join("");
   if (!answer) {
-    msg.textContent = 'Please enter a valid answer';
+    msg.textContent = "Please enter a valid answer";
   } else if (answer !== arranged && answer) {
-    msg.textContent = 'Wrong answer... Try again';
+    msg.textContent = "Wrong answer... Try again";
   } else if (answer === arranged) {
-    msg.textContent = 'Correct!';
+    msg.textContent = "Correct!";
     score++;
     elScore.textContent = `Score: ${score}`;
     jumbledWord = generateRandomJumbledWord();
@@ -47,15 +47,15 @@ buttonCheck.addEventListener('click', function (e) {
       elHighScore.textContent = `High: ${highscore}`;
     }
   }
-  inputAnswer.value = '';
+  inputAnswer.value = "";
 });
 
-buttonAgain.addEventListener('click', function (e) {
+buttonAgain.addEventListener("click", function (e) {
   e.preventDefault();
 
   jumbledWord = generateRandomJumbledWord();
-  msg.textContent = 'Start arranging...';
-  inputAnswer.value = '';
+  msg.textContent = "Start arranging...";
+  inputAnswer.value = "";
   score = 0;
   elScore.textContent = `Score: ${score}`;
 });
